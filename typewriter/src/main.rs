e4cce21a16641475b7324d70d239e04cccdae68c
Robert Lord <robert@lord.io>
2019-09-25 19:02:43 -0400

    cargo fmt

--- typewriter/src/main.rs
+++ typewriter/src/main.rs
@@ -1,6 +1,6 @@
+use rusttype::{self, Contour, Curve, FontCollection, Line, PositionedGlyph, Scale, Segment};
 use std::fs::File;
 use std::io::Read;
-use rusttype::{self, FontCollection, PositionedGlyph, Segment, Contour, Curve, Line, Scale};
 
 fn error(msg: &str) -> ! {
     eprintln!("{}", msg);
@@ -23,15 +23,20 @@ fn convert_point(point: rusttype::Point<f32>) -> Point {
 
 fn segment_to_points(segment: Segment) -> Vec<Point> {
     match segment {
-        Segment::Line(line) => {
-            vec![line.p[0], line.p[1]].into_iter().map(convert_point).collect()
-        }
+        Segment::Line(line) => vec![line.p[0], line.p[1]]
+            .into_iter()
+            .map(convert_point)
+            .collect(),
         Segment::Curve(curve) => {
             let mut res = vec![];
             for i in 0..11 {
                 let t = i as f32 * 0.1;
-                let x = (1.0 - t) * (1.0 - t) * curve.p[0].x + 2.0 * (1.0 - t) * t * curve.p[1].x + t * t * curve.p[2].x;
-                let y = (1.0 - t) * (1.0 - t) * curve.p[0].y + 2.0 * (1.0 - t) * t * curve.p[1].y + t * t * curve.p[2].y;
+                let x = (1.0 - t) * (1.0 - t) * curve.p[0].x
+                    + 2.0 * (1.0 - t) * t * curve.p[1].x
+                    + t * t * curve.p[2].x;
+                let y = (1.0 - t) * (1.0 - t) * curve.p[0].y
+                    + 2.0 * (1.0 - t) * t * curve.p[1].y
+                    + t * t * curve.p[2].y;
                 let point = Point {
                     x: x as i64,
                     y: y as i64,
@@ -65,20 +70,30 @@ fn main() {
     if args.len() < 6 {
         error("expected 5 arguments");
     }
-    let x: i64 = args[1].parse().unwrap_or_else(|_| error("expected first arg to be int"));
-    let y: i64 = args[2].parse().unwrap_or_else(|_| error("expected second arg to be int"));
-    let font_size: i64 = args[3].parse().unwrap_or_else(|_| error("expected third arg to be int"));
+    let x: i64 = args[1]
+        .parse()
+        .unwrap_or_else(|_| error("expected first arg to be int"));
+    let y: i64 = args[2]
+        .parse()
+        .unwrap_or_else(|_| error("expected second arg to be int"));
+    let font_size: i64 = args[3]
+        .parse()
+        .unwrap_or_else(|_| error("expected third arg to be int"));
     let msg: &str = &args[4];
     let path: &str = &args[5];
 
     let mut font_data: Vec<u8> = Vec::new();
     {
         let mut file = File::open(path).unwrap_or_else(|_| error("failed to open font file"));
-        file.read_to_end(&mut font_data).unwrap_or_else(|_| error("failed to read font file"));
+        file.read_to_end(&mut font_data)
+            .unwrap_or_else(|_| error("failed to read font file"));
     }
 
     let collection = FontCollection::from_bytes(&font_data).unwrap_or_else(|e| {
-        error(&format!("error constructing a FontCollection from bytes: {}", e));
+        error(&format!(
+            "error constructing a FontCollection from bytes: {}",
+            e
+        ));
     });
     let font = collection
         .into_font() // only succeeds if collection consists of one font
@@ -86,7 +101,16 @@ fn main() {
             error(&format!("error turning FontCollection into a Font: {}", e));
         });
 
-    let glyphs: Vec<PositionedGlyph<'_>> = font.layout(msg, Scale::uniform(font_size as f32), rusttype::Point {x: x as f32, y: y as f32}).collect();
+    let glyphs: Vec<PositionedGlyph<'_>> = font
+        .layout(
+            msg,
+            Scale::uniform(font_size as f32),
+            rusttype::Point {
+                x: x as f32,
+                y: y as f32,
+            },
+        )
+        .collect();
     for glyph in glyphs {
         let contours = match glyph.shape() {
             None => continue,
